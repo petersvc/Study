90b2fdf5316f846200d93203ac3935585e8ecaa9
Petersvc <petersvc@outlook.com>
2017-09-03 18:07:22 -0300

    php

---
+++ php/ola.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+  <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+  <h1>Estudando PHP</h1>
+  <?php
+    echo "Olá mundo! isso é um <i>echo</i>";
+  ?>
+  <?php
+    echo "<h2>h2 dentro de um <i>echo</i></h2>";
+  ?>
+</body>
+</html>
