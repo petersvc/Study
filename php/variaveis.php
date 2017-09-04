6c6cf58613a66588a3efffddd2ba57965c631c8e
Petersvc <petersvc@outlook.com>
2017-09-04 03:06:03 -0300

    variáveis e css

--- php/variaveis.php
+++ php/variaveis.php
@@ -4,16 +4,16 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Olá_mundo</title>
+  <title>Variáveis</title>
   <link rel="stylesheet" href="css/styles.css">
 </head>
 <body>
-  <h1>Estudando PHP</h1>
+  <h1>Variáveis em PHP</h1>
   <?php
-    echo "Olá mundo! isso é um <i>echo</i>";
-  ?>
-  <?php
-    echo "<h2>h2 dentro de um <i>echo</i></h2>";
+    $idade = 28;
+    $nome = "Peter";
+    $num_float = 5.25;
+    echo "$nome tem $idade anos";
   ?>
 </body>
 </html>
