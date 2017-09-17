30755042c2e4d204de2a01cb5b9004bfa42b758e
petersvc <petersvc@outlook.com>
2017-09-17 03:35:28 -0300

    javascript

---
+++ javascript/cap2.js
@@ -0,0 +1,8 @@
+var n1 = 10;
+var n2 = 25;
+var frase = "Olá mundo! ";
+document.getElementById("iten1").innerHTML = frase;
+document.getElementById("iten2").innerHTML = "n1 + n2 = " + (n1 + n2);
+document.getElementById("iten3").innerHTML = "n1 = " + n1 + ", n2 = " + n2 + ", n1 e n2 = " + n1 + n2;
+document.getElementById("iten4").innerHTML = "n1 = " + n1 + ", n2 = " + n2 + ", o maior é: " + Math.max(n1, n2);
+document.getElementById("iten5").innerHTML = "n1 = " + n1 + ", n2 = " + n2 + ", o maior é: " + Math.min(n1, n2);
