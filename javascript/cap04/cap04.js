752a57f771be2eea85537324c81c85cc6af12477
petersvc <petersvc@outlook.com>
2017-09-26 20:05:15 -0300

    cap04

--- javascript/cap04/cap04.js
+++ javascript/cap04/cap04.js
@@ -1,8 +1,9 @@
 var tam = 3;
 var ac = 1;
-var nota = [2,2,2];
+var nota = [];
+var fu = document.getElementById("fu");
 for (var i = 0; i < tam; i++) {
-    nota[i] += ac; 
-    console.log("Nota " + (i+1) + ": " + nota[i]);    
+    nota[i] = ac; 
+    fu.innerHTML += "Nota " + (i+1) + ": " + nota[i] + "<br/>";    
     ac+= 4;
 }
