0dd5faff8480431929879287e64cc7b8290a4153
petersvc <petersvc@outlook.com>
2017-10-01 17:37:47 -0300

    factorial

--- javascript/cap04/cap04.js
+++ javascript/cap04/cap04.js
@@ -3,8 +3,8 @@ var ac = 1;
 var nota = [];
 var fu = document.getElementById("fu");
 for (var i = 0; i < tam; i++) {
-    nota[i] = ac; 
-    fu.innerHTML += "Nota " + (i+1) + ": " + nota[i] + "<br/>";    
+    nota[i] = ac;
+    fu.innerHTML += "Nota " + (i+1) + ": " + nota[i] + "<br/>";
     ac+= 4;
 }
 
@@ -12,9 +12,9 @@ var fu2 = document.getElementById("fu2");
 var button = document.querySelector("#button");
 button.onclick = function(){
     var data = document.querySelector(".inp").value;
-    fu2.innerHTML += data;
+    fu2.innerText = data;
 };
 var button2 = document.querySelector("#button2");
 button2.onclick = function(){
-    fu2.innerHTML = this.innerHTML.toUpperCase();
+    fu2.innerText = fu2.innerText.toUpperCase();
 };
